45adc927b4514d594352023a6b1b42c9a195730f
Tuva12 <95386892+Tuva12@users.noreply.github.com>
2026-04-28 20:35:38 +0200

    Add title to Delivery Decision Support Tool
    Added title to the Delivery Decision Support Tool.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from scenarios import run_scenario_3, get_demo_dates_s3
 
+st.title("Delivery Decision Support Tool")
 date = st.selectbox("Date", get_demo_dates_s3())
 remove = st.number_input("Remove", 1, 5, 2)
 top = st.number_input("Top", 1, 15, 10)
